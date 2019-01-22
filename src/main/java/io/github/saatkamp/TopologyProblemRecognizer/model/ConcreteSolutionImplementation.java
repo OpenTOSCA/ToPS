510bce44c9c7e939250686724d9f0d84e7f80c5d
Karoline Saatkamp <karoline.saatkamp@iaas.uni-stuttgart.de>
2019-01-22 10:13:04 +0100

    fix  si
    Signed-off-by: Karoline Saatkamp <karoline.saatkamp@iaas.uni-stuttgart.de>

--- src/main/java/io/github/saatkamp/TopologyProblemRecognizer/model/ConcreteSolutionImplementation.java
+++ src/main/java/io/github/saatkamp/TopologyProblemRecognizer/model/ConcreteSolutionImplementation.java
@@ -2,16 +2,16 @@ package io.github.saatkamp.TopologyProblemRecognizer.model;
 
 public class ConcreteSolutionImplementation {
 
-    private String name;
+    private String algorithm;
     private String serviceEndpoint;
 
-    public ConcreteSolutionImplementation(String name, String serviceEndpoint) {
-        this.name = name;
+    public ConcreteSolutionImplementation(String algorithm, String serviceEndpoint) {
+        this.algorithm = algorithm;
         this.serviceEndpoint = serviceEndpoint;
     }
 
-    public String getName() {
-        return name;
+    public String getAlgorithm() {
+        return algorithm;
     }
 
     public String getServiceEndpoint() {
