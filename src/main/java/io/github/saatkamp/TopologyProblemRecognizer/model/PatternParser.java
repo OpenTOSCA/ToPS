5f6916598d54358f6f46529a58e1af957581b569
Karoline Saatkamp <karoline.saatkamp@iaas.uni-stuttgart.de>
2021-06-25 11:04:02 +0200

    add iterator for more text in pattern description to PatternParser
    Signed-off-by: Karoline Saatkamp <karoline.saatkamp@iaas.uni-stuttgart.de>

--- src/main/java/io/github/saatkamp/TopologyProblemRecognizer/model/PatternParser.java
+++ src/main/java/io/github/saatkamp/TopologyProblemRecognizer/model/PatternParser.java
@@ -3,6 +3,8 @@ package io.github.saatkamp.TopologyProblemRecognizer.model;
 import com.vladsch.flexmark.ast.Node;
 import com.vladsch.flexmark.ast.NodeIterator;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.collection.iteration.ReversiblePeekingIterable;
+import com.vladsch.flexmark.util.collection.iteration.ReversiblePeekingIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +27,16 @@ public class PatternParser {
         // Read Paragraph text of the second heading
         document = document.getNext().getNext();
         String problem = document.getFirstChild().getChars().toString();
+        boolean iterator = true;
+        while(iterator) {
+            if(document.getNext().getFirstChild() != null && !document.getNext().getFirstChild().getChars().toString().contains("Problem Rule")) {
+                problem = problem + " " + document.getNext().getFirstChild().getChars().toString();
+                document = document.getNext();
+            } else {
+                iterator = false;
+            }
+
+        }
         // Read FenceCodeBlock of the third heading
         document = document.getNext().getNext();
         String problemRule = document.getFirstChild().getChars().toString();
